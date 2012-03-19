1b571008091f8a155cbec696f37b8e0c68acc124
Augusto Pascutti <augusto@phpsp.org.br>
2012-03-19 03:13:19 -0300

    First test that generates its own documentation. How about that?

--- tests/library/Respect/Doc/GeneratorTest.php
+++ tests/library/Respect/Doc/GeneratorTest.php
@@ -2,60 +2,14 @@
 namespace Respect\Doc;
 use Respect\Doc\Reflection;
 
-class GeneratorExampleTest extends \PHPUnit_Framework_TestCase
+class GeneratorTest extends \PHPUnit_Framework_TestCase
 {
-	public function test_doc_for_reflection_class()
+	public function test_as_example_for___toString_method()
 	{
-		$class    = 'Respect\Doc\GeneratorMock';
-		$expected = <<<'MD'
-# Respect\Doc\GeneratorMock
-
-Generates the markdown for a given path.
-
-## something
-
-@var Foo
-
-## static zHelloFunction($foo, $bar, $baz=null)
-
-## __construct($classname)
-
-## __toString()
-MD;
+		$class     = 'Respect\Doc\Generator';
 		$generator = new Generator($class);
 		$markdown  = (string) $generator;
-		$this->assertEquals($expected, trim($markdown));
+		$doc       = file_put_contents('../README.md', $markdown); //ha
+		$this->assertStringEqualsFile('../README.md', $markdown);
 	}
-
-	public function test_as_example_for___toString()
-	{
-
-	}
-}
-
-/**
- * Generates the markdown for a given path.
- */
-class GeneratorMock
-{
-	/** @var Foo */
-	public $something;
-
-	public function __construct($classname)
-	{
-		$this->ns = $classname;
-	}
-
-	public function __toString()
-	{
-		$sections = array();
-		$md       = '';
-		return $this->getMarkdown($this->getSections());
-		
-	}
-
-	public static function zHelloFunction($foo, $bar, $baz=null) {
-
-	}
-
 }
