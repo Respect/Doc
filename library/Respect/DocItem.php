df4f6d5ab0f089668b2d81be2e9dc1dfee882f8b
Ivo Nascimento <iannsp@gmail.com>
2012-03-27 01:48:04 -0300

    extract getSections from Doc to here

--- library/Respect/DocItem.php
+++ library/Respect/DocItem.php
@@ -40,5 +40,43 @@ class DocItem
     {
         return call_user_func_array(array($this->refItem, $method),$parameters );
     }
+    public function getMethods($scope=null)
+    {
+        if (is_null($scope))
+            return $this->refItem->getMethods();
+        return $this->refItem->getMethods($scope);
+    }
+    public function getSections() 
+    {
+        $testCaseClass = $this->getName().'Test';
+
+        if (class_exists($testCaseClass)) {
+            $testCaseReflection = new ReflectionClass($testCaseClass);
+            $testCaseMethods = $testCaseReflection->getMethods();
+        } else {
+            $testCaseMethods= array();
+        }
+        $sections = new SplObjectStorage;
+        $methods = $this->getMethods(ReflectionMethod::IS_PUBLIC ^ ReflectionMethod::IS_STATIC);
+        $properties = $this->getProperties(ReflectionProperty::IS_PUBLIC ^ ReflectionProperty::IS_STATIC);
+        $staticMethods = $this->getMethods(ReflectionMethod::IS_PUBLIC & ReflectionMethod::IS_STATIC);
+        $staticProperties = $this->getProperties(ReflectionProperty::IS_PUBLIC & ReflectionProperty::IS_STATIC);
+
+        $nameSort = function($a, $b) {
+            return strnatcasecmp($a->getName(), $b->getName());
+        };
+
+        usort($methods, $nameSort);
+        usort($properties, $nameSort);
+        usort($staticMethods, $nameSort);
+        usort($staticProperties, $nameSort);
+        foreach (array_merge($staticProperties, $properties, $staticMethods, $methods) as $method)
+            $sections[$method] = array_values(array_filter($testCaseMethods, function($test) use($method) {
+                                    return 0 === stripos($test->getName(), 'test_as_example_for_'.$method->getName().'_method');
+                                 }));
+        return $sections;
+    }
+
+
 }
 
