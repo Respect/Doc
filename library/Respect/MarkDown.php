bd5d233016be1b7bec95d55c21dff308c758b5b9
Ivo Nascimento <iannsp@gmail.com>
2012-03-27 03:47:48 -0300

    extract getMarkDown from Doc to here

---
+++ library/Respect/MarkDown.php
@@ -0,0 +1,29 @@
+<?php
+namespace Respect;
+
+class MarkDown
+{
+    public function get(array $sections)
+    {
+        $string = array();
+        foreach ($sections as $name=>$content) {
+            
+            if (preg_match_all('/[\:]{1,2}(.*)/', $name, $matches))
+                $name = $matches[1][0];
+            else 
+                $matches = 1;
+
+            $content  = trim(preg_replace('#^(\s*[*]|[/][*]{2}|[\][*])[/*]*(.*?)[ /*]*$#m', '$2', $content));
+            $content  = preg_replace("#\\n\\n[ ]*@#", "\n\nMore Info:\n\n@", $content);
+            $content  = preg_replace_callback('#^[ ]*[@](\w+)[ ]+(.*)#mi', 
+                function($matches){
+                    $matches[1] = ucfirst($matches[1]);
+                    return "   - **{$matches[1]}:** {$matches[2]} ";
+                }, 
+            $content);
+            $char   = count($matches) == 1 ? '=' : '-';
+            $string[] = trim($name . "\n" .  str_repeat($char, strlen($name)) . "\n\n" . $content);
+        }
+        return implode("\n\n", $string);
+    }
+}
