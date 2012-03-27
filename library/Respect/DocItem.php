3b29e9103602065a95321231a8797df98d8a30cd
Ivo Nascimento <iannsp@gmail.com>
2012-03-27 00:34:39 -0300

    support reflection class for analize

---
+++ library/Respect/DocItem.php
@@ -0,0 +1,37 @@
+<?php
+namespace Respect;
+
+use \SplObjectStorage;
+use \ReflectionClass;
+use \ReflectionMethod;
+use \ReflectionProperty;
+
+
+/**
+ * DocItem Reflection Class to speak where is the socks to rock it.  
+ * 
+ * @author Ivo Nascimento <ivo.nascimento@php.net> 
+ */
+class DocItem
+{
+    private $item;
+    private $refItem;
+
+    /**
+     * __construct Construct a doc Item 
+     * 
+     * @access public
+     * @return void
+     */
+    public function __construct($item)
+    {
+       $this->docItem =  $item;
+       $this->refItem = new ReflectionClass($item);
+    }
+    public function getName()
+    {
+        return $this->refItem->getName();
+    }
+
+}
+
