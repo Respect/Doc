cca9df03f51e6258209a4bae55fdd512fbabdbd0
Ivo Nascimento <iannsp@gmail.com>
2012-03-31 20:40:18 -0300

    add verify to get

--- tests/library/Respect/Doc/NameSpaceAnalizerTest.php
+++ tests/library/Respect/Doc/NameSpaceAnalizerTest.php
@@ -3,13 +3,19 @@ namespace Respect\Doc;
                                                  
 class NameSpaceAnalizerTest extends \phpunit_framework_testcase
 {
-    private $namespace;
+    private $ns;
     public function setUp()
     {
-        $this->namespace = new NameSpaceAnalizer();
+        $this->ns = new NameSpaceAnalizer();
     }
     public function tearDown()
     {
         $this->namespace = null;
+    }
+    public function testget()
+    {
+        $class                          =  new MarkDown();
+        $arrayWithAllFilesInNameSpace   = $this->ns->get($class);
+        $this->AssertCount(4, $arrayWithAllFilesInNameSpace);
     }
 }
