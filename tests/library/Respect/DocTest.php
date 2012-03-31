df8a37ebe2124512e462b4a2a6eca0142cfd9367
Ivo Nascimento <iannsp@gmail.com>
2012-03-31 19:25:09 -0300

    change namespace to Respect\Doc

--- tests/library/Respect/DocTest.php
+++ tests/library/Respect/DocTest.php
@@ -1,5 +1,5 @@
 <?php
-namespace Respect;
+namespace Respect\Doc;
 
 class DocTest extends \PHPUnit_Framework_TestCase
 {
@@ -14,23 +14,23 @@ class DocTest extends \PHPUnit_Framework_TestCase
     public function test_as_example_for___construct_method_with_namespace()
     {
         $doc    = new Doc('Respect'); //Full Namespace
-        $title  = 'Respect\Doc' . PHP_EOL 
+        $title  = 'Respect\Doc\Doc' . PHP_EOL 
                 . '===========';
         $this->assertStringStartsWith($title, (string) $doc);
     }
     public function test_as_example_for___construct_method_with_class()
     {
         $doc    = new Doc('Respect\Doc'); //Specific |class Name
-        $title  = 'Respect\Doc' . PHP_EOL 
+        $title  = 'Respect\Doc\Doc' . PHP_EOL 
                 . '===========';
         $this->assertStringStartsWith($title, (string) $doc);
     }
     public function test_getMarkDownforNamespace()
     {
-        $doc    = new Doc('Respect'); //Specific |class Name
+        $doc    = new Doc('Respect');
         $content =  (string)$doc;
-        $this->assertGreaterThanOrEqual( 100, strpos($content,'Respect\MarkDown'));
-        $this->assertGreaterThanOrEqual( 50, strpos($content,'Respect\DocItem'));
-        $this->assertEquals( 0, strpos($content,'Respect\Doc'));
+        $this->assertGreaterThanOrEqual( 100, strpos($content,'Respect\\Doc\MarkDown'));
+        $this->assertGreaterThanOrEqual( 50, strpos($content,'Respect\Doc\DocItem'));
+        $this->assertEquals( 0, strpos($content,'Respect\Doc\Doc'));
     }
 }
--- tests/library/Respect/MarkDownTest.php
+++ tests/library/Respect/MarkDownTest.php
@@ -1,12 +1,12 @@
 <?php
-namespace Respect;                               
+namespace Respect\Doc;                               
                                                  
 class MarkDownTest extends \phpunit_framework_testcase
 {
     public function testget()
     {
 
-        $doc = new DocItem('\Respect\Doc');
+        $doc = new DocItem('\Respect\Doc\Doc');
         $markdown = new MarkDown();
         $content = \file_get_contents (__DIR__."/../../output/RespectDoc_output.txt");
         $sections = (array)$doc->getClassContent();
