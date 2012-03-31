1e9591a1d962a7dbcab63312345523c2cf9885d0
Ivo Nascimento <iannsp@gmail.com>
2012-03-31 20:39:41 -0300

    add toDirectory

--- library/Respect/Doc/NameSpaceAnalizer.php
+++ library/Respect/Doc/NameSpaceAnalizer.php
@@ -6,7 +6,28 @@ namespace Respect\Doc;
 class NameSpaceAnalizer
 {
     
+/**
+ * undocumented function
+ *
+ * @return void
+ * @author Ivo Nascimento
+ **/
     public function get($class) {
+        $directory = $this->toDirectory(get_class($class));
+        return $this->toDirectory(get_class($class));
+    }
+    private function toDirectory($className)
+    {
+        $file   = str_replace('\\','/',$className).".php";
+        foreach (explode(PATH_SEPARATOR, get_include_path()) as $path) {
+            if (file_exists($path = $path . DIRECTORY_SEPARATOR . $file)) {
+                return new \RecursiveDirectoryIterator(dirname($path));
+            }
+        }
+        throw new \Exception("Have no include_path to {$class}");
+    }
+    private function getClasses(\RecursiveDirectoryIterator $directory)
+    {
         
     }
     
