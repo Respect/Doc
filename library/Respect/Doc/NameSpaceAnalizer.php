f967906015f478dab6913cfbc637f689f1a7103f
Ivo Nascimento <iannsp@gmail.com>
2012-03-31 21:48:54 -0300

    start change to string Namespace

--- library/Respect/Doc/NameSpaceAnalizer.php
+++ library/Respect/Doc/NameSpaceAnalizer.php
@@ -13,12 +13,15 @@ class NameSpaceAnalizer
  * @author Ivo Nascimento
  **/
     public function get($class) {
-        $directory = $this->toDirectory(get_class($class));
+        //if (is_object($class)) {
+            $directory = $this->toDirectory(get_class($class).".php");
+        //}
+        
         return $directory;
     }
     private function toDirectory($className)
     {
-        $file   = str_replace('\\','/',$className).".php";
+        $file   = str_replace('\\','/',$className);
         foreach (explode(PATH_SEPARATOR, get_include_path()) as $path) {
             if (file_exists($path = $path . DIRECTORY_SEPARATOR . $file)) {
                 return new \DirectoryIterator(dirname($path));
