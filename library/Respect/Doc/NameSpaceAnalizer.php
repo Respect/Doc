65b458b654f1271be1196bb646eb5fa3aaf788c3
Ivo Nascimento <iannsp@gmail.com>
2012-03-31 22:16:52 -0300

    add a test to file or directory

--- library/Respect/Doc/NameSpaceAnalizer.php
+++ library/Respect/Doc/NameSpaceAnalizer.php
@@ -16,13 +16,16 @@ class NameSpaceAnalizer
         if (is_object($target)) {
             return $this->toDirectory(get_class($target).".php");
         }
-        return $this->toDirectory($target);
+        return $this->toDirectory($target, true);
     }
     private function toDirectory($target)
     {
         $file   = str_replace('\\','/',$target);
         foreach (explode(PATH_SEPARATOR, get_include_path()) as $path) {
             if (file_exists($path = $path . DIRECTORY_SEPARATOR . $file)) {
+                if (!is_int(strpos($target, ".php"))){
+                    return new \DirectoryIterator($path);
+                }
                 return new \DirectoryIterator(dirname($path));
             }
         }
