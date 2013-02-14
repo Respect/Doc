1dde776067db4b60e2413f27d03c799c3bd97cbd
Augusto Pascutti <augusto@phpsp.org.br>
2013-02-14 00:24:35 -0200

    Improves tests for Respect\Doc\DocItem.

--- tests/src/Respect/Doc/DocItemTest.php
+++ tests/src/Respect/Doc/DocItemTest.php
@@ -1,59 +1,123 @@
 <?php
+
 namespace Respect\Doc;
 
-class DocItemTest extends \phpunit_framework_testcase
+use ReflectionClass;
+
+class DocItemTest extends \PHPUnit_Framework_TestCase
 {
-    private $docItem;
-    public function setUp()
+    /**
+     * Class to be used as an item to be documented on these tests.
+     * If you change this, you are "probably" going to change some assertions also.
+     */
+    const DOC_ITEM_CLASS = 'Respect\Doc\DocItem';
+
+    /**
+     * @covers Respect\Doc\DocItem::__construct
+     */
+    public function test_new_instance_with_class_name()
     {
-        $this->docItem = new DocItem( 'Respect\Doc\DocItem');
+        $this->assertInstanceOf(
+            $expected = 'Respect\Doc\DocItem',
+            $result   = new DocItem(self::DOC_ITEM_CLASS)
+        );
     }
-    public function tearDown()
+
+    /**
+     * @covers Respect\Doc\DocItem::__construct
+     * @expectedException           PHPUnit_Framework_Error
+     * @expectedExceptionMessage    Missing argument 1 for
+     */
+    public function test_new_instance_without_argument_fails()
     {
-        $this->docItem = null;
+        $this->assertInstanceOf(
+            $expected = 'Respect\Doc\DocItem',
+            $result   = new DocItem
+        );
     }
-    public function test_getName()
+
+    /**
+     * @covers Respect\Doc\DocItem::getName
+     */
+    public function test_getName_returns_the_name_of_the_class()
     {
-        $this->AssertEquals('Respect\Doc\DocItem', $this->docItem->getName());
+        $item = new DocItem(self::DOC_ITEM_CLASS);
+        $this->AssertEquals('Respect\Doc\DocItem', $item->getName());
     }
-    public function test_getDocComment()
+
+    /**
+     * @covers Respect\Doc\DocItem::getDocComment
+     */
+    public function test_get_phpdoc_from_class()
     {
-        $returnValue = <<<PHPDOC
+        $item     = new DocItem(self::DOC_ITEM_CLASS);
+        $expected = <<<PHPDOC
 /**
  * DocItem Reflection Class to speak where is the socks to rock it.
  *
  * @author Ivo Nascimento <ivo.nascimento@php.net>
  */
 PHPDOC;
-        $this->AssertEquals($returnValue, $this->docItem->getDocComment());
+        $this->assertEquals(
+            $expected,
+            $result = $item->getDocComment()
+        );
 }
 
-    public function getMethodsDataprovider()
-{
-        return array(
-                array(0,'__construct'),
-                array(1,'getName'),
-                array(2,'getDocComment'),
-                array(3,'__call')
-        );
-    }
     /**
-     * @dataProvider getMethodsDataprovider
+     * @covers Respect\Doc\DocItem::getMethods
      */
-    public function test_getMethods($idx, $name)
+    public function test_getMethods_retrieves_all_method_names()
     {
-        $methods = $this->docItem->getMethods();
-        $this->AssertEquals($methods[$idx]->name, $name );
+        $item       = new DocItem(self::DOC_ITEM_CLASS);
+        $reflection = new ReflectionClass(self::DOC_ITEM_CLASS);
+        $this->assertEquals(
+            count($expected = $reflection->getMethods()),
+            count($result   = $item->getMethods()),
+            'Expected a different quantity of methods as result.'
+        );
+        $this->assertContainsOnlyInstancesOf(
+            'ReflectionMethod',
+            $result,
+            'Resulting methods is not an array of ReflectionMethod instances.'
+        );
+        // Converts an array of ReflectionMethods into an array of strings with method names only.
+        $conversor       = function($reflectionMethod) { return $reflectionMethod->getName(); };
+        $expectedMethods = array_map($conversor, $expected);
+        // For every ReflectionMethod instance returned
+        foreach ($result as $resultingMethod)
+            $this->assertContains(
+                $expectedMethodName = $resultingMethod->getName(),
+                $expectedMethods,
+                sprintf('Expected method "%s" to exist into class "%s"', $expectedMethodName, $reflection->getName())
+            );
     }
+
+    /**
+     * @TODO Improve name and code coverage of this test.
+     * @covers Respect\Doc\DocItem::getSections
+     */
     public function test_getSections()
     {
-        $this->AssertInstanceOf('SplObjectStorage',$this->docItem->getSections());
+        $item = new DocItem(self::DOC_ITEM_CLASS);
+        $this->AssertInstanceOf(
+            'SplObjectStorage',
+            $item->getSections()
+        );
     }
+
+    /**
+     * @TODO Improve test name reducing the code covered by this test.
+     * @covers Respect\Doc\DocItem
+     * @covers Respect\Doc\Doc
+     */
     public function test_getClassContents()
     {
-        $reference = __DIR__."/../../../output/RespectDoc_output.txt";
-        $doc = new Doc('\Respect\Doc\Doc');
-        $content = \file_get_contents (__DIR__."/../../../output/RespectDoc_output.txt");
-        $this->AssertEquals($content, (string)$doc);
+        $doc      = new Doc('\Respect\Doc\Doc');
+        $expected = file_get_contents("RespectDoc_output.txt", $useIncludePath=true);
+        $this->assertEquals(
+            $expected,
+            $result = (string)$doc
+        );
     }
 }
