7b919e220d7ad70966de3caff54a005e4ae17b17
Augusto Pascutti <augusto@phpsp.org.br>
2013-02-13 23:22:19 -0200

    Tests no longer change README.md

--- tests/src/Respect/Doc/DocTest.php
+++ tests/src/Respect/Doc/DocTest.php
@@ -8,8 +8,8 @@ class DocTest extends \PHPUnit_Framework_TestCase
 		$class     = 'Respect\Doc';
 		$doc       = new Doc($class);
 		$markdown  = (string) $doc;
-		$doc       = file_put_contents('../README.md', $markdown); //Happy Panda
-		$this->assertStringEqualsFile('../README.md', $markdown);
+		$doc       = file_put_contents('/tmp/README.md', $markdown); //Happy Panda
+		$this->assertStringEqualsFile('/tmp/README.md', $markdown);
 	}
     public function test_as_example_for___construct_method_with_namespace()
     {
